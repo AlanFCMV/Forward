d620226e53a819c3efe69a8695b999d3a1617984
AlanFCMV <verdu.alan@gmail.com>
2021-09-25 16:36:53 -0400

    Add Task foreignKey

--- app/src/main/java/com/forward/app/models/Task.java
+++ app/src/main/java/com/forward/app/models/Task.java
@@ -2,9 +2,20 @@ package com.forward.app.models;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "tasks")
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "tasks",
+        foreignKeys = @ForeignKey(
+                entity = Goal.class,
+                parentColumns = "id",
+                childColumns = "goalId",
+                onDelete = CASCADE,
+                onUpdate = CASCADE
+        )
+)
 public class Task {
     @PrimaryKey(autoGenerate = true)
     public int id;
