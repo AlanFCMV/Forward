534bbfef705676f7f63648c9ce3b4e937d9db553
AlanFCMV <verdu.alan@gmail.com>
2021-09-25 16:27:25 -0400

    Add query for TaskWithNotifications to TaskDao.java

--- app/src/main/java/com/forward/app/dao/TaskDao.java
+++ app/src/main/java/com/forward/app/dao/TaskDao.java
@@ -4,14 +4,20 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.forward.app.models.Task;
+import com.forward.app.models.TaskWithNotifications;
 
 import java.util.List;
 
 @Dao
 public interface TaskDao {
+    @Transaction
+    @Query("SELECT * FROM tasks WHERE id = :taskId")
+    TaskWithNotifications getTaskWithNotifications(int taskId);
+
     @Insert
     void insertTasks(Task... tasks);
 
