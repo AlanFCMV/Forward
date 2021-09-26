c44c6c52d93db697250cea0c10d28c92334973dd
noah-lasagna <noah.castagna@Knights.ucf.edu>
2021-09-25 20:30:13 -0400

    Added View Goal xml and Activity

---
+++ app/src/main/java/com/forward/app/ViewGoalActivity.java
@@ -0,0 +1,17 @@
+package com.forward.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_goal);
+    }
+
+
+
+}
